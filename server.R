9f30d80bdf4535103b6836a218d62649dc336b81
Browns5010 <71524264+Browns5010@users.noreply.github.com>
2020-12-08 10:39:47 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -14,29 +14,37 @@ shinyServer(function(input, output, session) {
   },
   once=TRUE)
   
-  output$plot <- renderPlot({
-    if(input$team == "All Teams")p <- ggplot(data=Baseball %>% filter(yearID >= input$year[1], 
-                               yearID <= input$year[2]),
-          aes(x=.data[[input$xvar]],y=.data[[input$yvar]])) + geom_point()
-    
-    if(input$team != "All Teams")p <- ggplot(data=Baseball %>% filter(franchID == input$team) %>% 
-                                                filter(yearID >= input$year[1], 
-                                                       yearID <= input$year[2]), 
-                                               aes(x=.data[[input$xvar]],y=.data[[input$yvar]])) + 
-        geom_point(size=4) 
-    
-    if(input$lines == 1) p <- p + geom_smooth(se=FALSE) 
-    
-    if(input$lines == 2) p <- p + geom_smooth(method="lm", se=FALSE)
-
-    return(p)
-  })
-  
   output$data <- renderDT({
     df <- Baseball
     updateSelectInput(session,"xvar",choices=colnames(df))
     updateSelectInput(session,"yvar", choices=colnames(df), selected="ERA")
     return(df)
+  })
+  
+  output$plot <- renderPlot({
+    if(input$team == "All Teams") ({ p <- ggplot(data=Baseball %>% filter(yearID >= input$year[1], 
+                               yearID <= input$year[2]),
+          aes(x=.data[[input$xvar]],y=.data[[input$yvar]])) + geom_point()
+    
+    if(input$lines == 1 ) p <- p + geom_smooth(se=FALSE)
+    
+    if(input$lines == 2)  p <- p + geom_smooth(method="lm", se=FALSE)
+    
+    })
+    
+    else ({ p <- ggplot(data=Baseball %>% filter(franchID == input$team) %>% 
+                                                filter(yearID >= input$year[1], 
+                                                       yearID <= input$year[2]), 
+                                               aes(x=.data[[input$xvar]],y=.data[[input$yvar]])) + 
+        geom_point(size=4, aes(color=franchID)) 
+    
+      if(input$lines == 1) p <- p + geom_smooth(se=FALSE)  + facet_wrap(vars(franchID))
+    
+      if(input$lines == 2) p <- p + geom_smooth(method="lm", se=FALSE) + facet_wrap(vars(franchID))
+      
+    })
+
+    return(p)
   })
   
 })
--- ui.R
+++ ui.R
@@ -9,18 +9,18 @@ dashboardPage(
     sliderInput("year","Select A Season Range", min=1900, max=2020, value=c(1900,2020), sep=""),
     selectInput("xvar","Select An X Variable", choices="Choose One of the Following"),
     selectInput("yvar","Select A Y Variable", choices="Choose One of the Following"),
-    selectInput("team","Select A Team", choices="All Teams", selected = "All Teams"),
+    selectInput("team","Select A Team", choices="All Teams", selected = "All Teams", multiple=TRUE),
     radioButtons("lines","Select a Fit",choices=list("Smooth"=1, "Linear"=2), selected=1)
     ),
   dashboardBody(
     fluidRow(
-      box(width=6, 
+      box(width=11,
           status="info", 
-          title="MLB Plot by Team and Year",
+          title="MLB Data Plot",
           solidHeader = TRUE,
           plotOutput("plot")
       ),
-      box(width=6,
+      box(width=12,
           status="info",
           title="MLB Data Frame",
           solidHeader=TRUE,
