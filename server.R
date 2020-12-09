0097baa6c710d7e7d10a336c91bfa7001247862d
Browns5010 <71524264+Browns5010@users.noreply.github.com>
2020-12-09 12:22:15 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -10,41 +10,82 @@ shinyServer(function(input, output, session) {
   Baseball <- read_csv("./data/BaseballTeams.csv")
   
   observeEvent(input$team, {
-    updateSelectInput(session, "team", choices=c("All Teams", unique(Baseball$franchID)), selected="All Teams")
+    updateSelectInput(session, "team", choices=c("All Teams", unique(Baseball$Team)), selected="All Teams")
   },
   once=TRUE)
   
   output$data <- renderDT({
     df <- Baseball
     updateSelectInput(session,"xvar",choices=colnames(df))
-    updateSelectInput(session,"yvar", choices=colnames(df), selected="ERA")
+    updateSelectInput(session,"yvar", choices=colnames(df),selected="EarnedRunAverage")
     return(df)
   })
   
   output$plot <- renderPlot({
-    if(input$team == "All Teams") ({ p <- ggplot(data=Baseball %>% filter(yearID >= input$year[1], 
-                               yearID <= input$year[2]),
-          aes(x=.data[[input$xvar]],y=.data[[input$yvar]])) + geom_point()
+    if(input$team == "All Teams") ({ p <- ggplot(data=Baseball %>% filter(Season >= input$year[1], 
+                               Season <= input$year[2]),
+          aes(x=.data[[input$xvar]],y=.data[[input$yvar]])) + geom_point(alpha=.4) + theme_minimal()
     
-    if(input$lines == 1 ) p <- p + geom_smooth(se=FALSE)
+    if(input$lines == 1 ) p <- p + geom_smooth(se=FALSE, color="red")
     
-    if(input$lines == 2)  p <- p + geom_smooth(method="lm", se=FALSE)
+    if(input$lines == 2)  p <- p + geom_smooth(method="lm", se=FALSE, color="red")
     
     })
     
-    else ({ p <- ggplot(data=Baseball %>% filter(franchID == input$team) %>% 
-                                                filter(yearID >= input$year[1], 
-                                                       yearID <= input$year[2]), 
+    else ({ p <- ggplot(data=Baseball %>% filter(Team == input$team) %>% 
+                                                filter(Season >= input$year[1], 
+                                                       Season <= input$year[2]), 
                                                aes(x=.data[[input$xvar]],y=.data[[input$yvar]])) + 
-        geom_point(size=4, aes(color=franchID)) 
+        geom_point(size=4, aes(color=Team)) + theme_minimal()
     
-      if(input$lines == 1) p <- p + geom_smooth(se=FALSE)  + facet_wrap(vars(franchID))
+      if(input$lines == 1) p <- p + geom_smooth(se=FALSE)  + facet_wrap(vars(Team))
     
-      if(input$lines == 2) p <- p + geom_smooth(method="lm", se=FALSE) + facet_wrap(vars(franchID))
+      if(input$lines == 2) p <- p + geom_smooth(method="lm", se=FALSE) + facet_wrap(vars(Team))
       
     })
 
     return(p)
+  })
+  
+  output$histx <- renderPlot ({
+    if(input$team == "All Teams") 
+       p <- ggplot(data=Baseball %>% filter(Season >= input$year[1], 
+                                              Season <= input$year[2]),
+                                                aes(x=.data[[input$xvar]])) + 
+        geom_histogram() + theme_classic() + theme(axis.title.y=element_blank(),
+                                                   axis.ticks.y=element_blank(),
+                                                   axis.text.y=element_blank())
+    
+    else p <- ggplot(data=Baseball %>% filter(Team == input$team) %>% 
+                          filter(Season >= input$year[1], 
+                                 Season <= input$year[2]), 
+                        aes(x=.data[[input$xvar]])) + 
+      geom_histogram() + theme_classic() + theme(axis.title.y=element_blank(),
+                                                 axis.ticks.y=element_blank(),
+                                                 axis.text.y=element_blank())
+    
+    return(p)
+  })
+  
+  output$histy <- renderPlot ({
+    if(input$team == "All Teams") 
+      p <- ggplot(data=Baseball %>% filter(Season >= input$year[1], 
+                                           Season <= input$year[2]),
+                  aes(x=.data[[input$yvar]])) + 
+        geom_histogram() + theme_classic() + theme(axis.title.y=element_blank(),
+                                                   axis.ticks.y=element_blank(),
+                                                   axis.text.y=element_blank())
+    
+    else p <- ggplot(data=Baseball %>% filter(Team == input$team) %>% 
+                       filter(Season >= input$year[1], 
+                              Season <= input$year[2]), 
+                     aes(x=.data[[input$yvar]])) + 
+        geom_histogram() + theme_classic() + theme(axis.title.y=element_blank(),
+                                                   axis.ticks.y=element_blank(),
+                                                   axis.text.y=element_blank())
+    
+    return(p)
+    
   })
   
 })
--- ui.R
+++ ui.R
@@ -7,22 +7,34 @@ dashboardPage(
   dashboardHeader(title = "My Dashboard"),
   dashboardSidebar(
     sliderInput("year","Select A Season Range", min=1900, max=2020, value=c(1900,2020), sep=""),
-    selectInput("xvar","Select An X Variable", choices="Choose One of the Following"),
-    selectInput("yvar","Select A Y Variable", choices="Choose One of the Following"),
+    selectInput("xvar","Select An X Variable", choices=NULL),
+    selectInput("yvar","Select A Y Variable", choices=NULL),
     selectInput("team","Select A Team", choices="All Teams", selected = "All Teams", multiple=TRUE),
     radioButtons("lines","Select a Fit",choices=list("Smooth"=1, "Linear"=2), selected=1)
     ),
   dashboardBody(
     fluidRow(
-      box(width=11,
+      box(width=6,
           status="info", 
           title="MLB Data Plot",
           solidHeader = TRUE,
           plotOutput("plot")
       ),
-      box(width=12,
+      box(width=6,
           status="info",
-          title="MLB Data Frame",
+          title="MLB Y Variable Histogram",
+          solidHeader=TRUE,
+          plotOutput("histy")
+      ),
+      box(width=6,
+          status="info",
+          title="MLB X Variable Histogram",
+          solidHeader=TRUE,
+          plotOutput("histx")
+      ),
+      box(width=6,
+          status="info",
+          title="MLB Data Table",
           solidHeader=TRUE,
           DTOutput("data"))
   )
